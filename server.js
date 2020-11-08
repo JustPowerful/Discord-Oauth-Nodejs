bbd6ff2140fadeb5ccb447853eb9aafa20c29c67
Ahmed Amine Doudech <hmidetocraft@gmail.com>
2020-11-08 23:47:46 +0100

    Nice and good for your health

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 const express = require('express')
-const app = express()
+const dotenv = require('dotenv').config()
+const fetch = require('node-fetch') // Fetch
+const app = express() // EXPRESS APP NODEJS
 
-app.get('/', (req, res) => {
-    res.send("Hello World")
+app.get('/login', async (req, res) => {
+    const data = {
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        grant_type: 'authorization_code',
+        redirect_uri: 'http://localhost:3000/login',
+        code: req.query.code,
+        scope: 'identify guilds',
+    };
+
+    let response = await fetch('https://discord.com/api/oauth2/token', {
+        method: 'POST',
+        body: new URLSearchParams(data),
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    })
+    let user = await response.json()
+
+    if (user) {
+        let response = await fetch('https://discord.com/api/users/@me', {
+            headers: {
+                authorization: `${user.token_type} ${user.access_token}`
+            }
+        })
+        res.json(await response.json())
+    }
 })
 
 
